e5bf0f2750b116222037aa13afb9e04f05407a4a
b3rg3t <berget_90db@hotmail.com>
2024-07-14 20:25:50 +0200

    test with env

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import { MainContent } from "../../components/main/MainContent"
 
 export const Home = () => {
+    console.log(import.meta.env.VITE_API_KEY, import.meta.env)
     return (
        <MainContent />
     )
