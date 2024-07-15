cc9f8bbae547c796312efdea37808787fc0115fe
b3rg3t <berget_90db@hotmail.com>
2024-07-15 20:11:49 +0200

    theming bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux"
 import { store } from "./store/store"
 import { RouterProvider } from "react-router-dom"
 import { router } from "./router/Routes"
-import "./App.scss"
+import "./styles/global.scss"
 
 const container = document.getElementById("root")
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,3 @@
 import { MainContent } from "../../components/main/MainContent"
 
-export const Home = () => {
-    console.log(import.meta.env.VITE_API_KEY, import.meta.env)
-    return (
-       <MainContent />
-    )
-}
+export const Home = () => <MainContent />
